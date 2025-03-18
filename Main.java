3b2f17d808db3f651d7474ae0c0cbdbcfd81b230
BrixtonKing <bk7832@pleasantonusd.net>
2025-03-18 22:10:10 +0000

    ;

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main (String[] args){
         System.out.println("hi, testing");     
-        System.out.println("Thai Change")                
+        System.out.println("Thai Change")   ;             
     }
 }
