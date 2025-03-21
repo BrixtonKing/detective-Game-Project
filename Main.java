58a770dafa77718e01334fc0542e5f64a6f603a8
allentong111 <bt0348@pleasantonusd.net>
2025-03-21 21:52:06 +0000

    random change

--- Main.java
+++ Main.java
@@ -4,8 +4,8 @@ public class Main {
     public static void main (String[] args){
 
         // testing code
-        Person brixton = new Person("Brixton", 16, "6' 0", true, 100000.1);
-        detectiveAgen tester = new detectiveAgen(10);
+        Person brixton = new Person("Brixton", 16, "6'0", true, 100000.1);
+        detectiveAgency tester = new detectiveAgency(10);
         // line 9 is needed becuase the arraylist is made as part fo the agency's constructor
         ArrayList<Suspect> banana = tester.getSuspects();
         // uses the to string from person
--- detectiveAgency.java
+++ detectiveAgency.java
@@ -1,10 +1,10 @@
 import java.util.ArrayList;
 
-public class detectiveAgen{
+public class detectiveAgency{
     // instance variables
     private ArrayList<Suspect> suspects = new ArrayList<>();
     private int suspectNumber;
-    public detectiveAgen(int suspectNumber){
+    public detectiveAgency(int suspectNumber){
         // basic set up for what will be our arraylist of random suspects
         this.suspectNumber = suspectNumber;
         for(int i = 0; i < suspectNumber; i++){
